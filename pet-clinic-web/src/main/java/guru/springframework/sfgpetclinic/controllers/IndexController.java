0db7a7c0851d60cfd7da2fc64c111cb77a22ffa3
Ihor Sukalin <naslednikg@gmail.com>
2023-01-03 22:14:01 +0200

    Add Pet Clinic Index Page and Controller. Closes #8.

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Ihor Sukalin on 03.01.2023
+ *
+ * @author : Ihor Sukalin
+ * date : 03.01.2023
+ * project : svg-pet-clinic
+ */
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/","index", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
