64b84f7105b065d76bb7e838dba595a01b77b5c4
Ihor Sukalin <naslednikg@gmail.com>
2023-01-22 08:59:09 +0200

    added PetTypeService. Closes #29.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * Created by Sukalin Ihor on 22.01.2023
+ *
+ * @author : Sukalin Ihor
+ * date : 22.01.2023
+ * project : svg-pet-clinic
+ */
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
